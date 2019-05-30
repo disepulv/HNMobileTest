a6ec18aee3fb4742c5e4b79b2e0d3e091b725c85
Diego Sepúlveda <dsepulveda@movix.com>
2019-05-30 01:45:51 -0400

    adds project configurations

---
+++ HNMobileTest/HNMobileTest-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  HNMobileTest-Bridging-Header.h
+//  HNMobileTest
+//
+//  Created by Diego Sepúlveda on 5/29/19.
+//  Copyright © 2019 reigndesign. All rights reserved.
+//
+
+#ifndef HNMobileTest_Bridging_Header_h
+#define HNMobileTest_Bridging_Header_h
+
+#import "iToast/iToast.h"
+#import <Reachability/Reachability.h>
+
+#endif /* HNMobileTest_Bridging_Header_h */
